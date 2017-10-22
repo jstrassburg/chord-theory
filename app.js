df464c619e9b179be0aa4634574cb9b51ba388c8
JiM Strassburg <jstrassburg@gmail.com>
2017-10-22 10:05:35 -0500

    adding bootstrap/angular, starter controller

---
+++ app.js
@@ -0,0 +1,5 @@
+angular.module('app', [])
+
+.controller('ChordTheory', function($scope){
+    $scope.allKeys = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+});

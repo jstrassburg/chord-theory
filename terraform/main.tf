4be18471377fe7ba82116ae4750310f4314da66c
JiM Strassburg <jstrassburg@gmail.com>
2017-10-22 16:07:16 -0500

    adding terraform to launch website

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::js-chord-theory-website/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "js-chord-theory-website"
+  acl    = "public-read"
+  policy = "${data.aws_iam_policy_document.s3_bucket_policy.json}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "app_js" {
+  bucket = "${aws_s3_bucket.website_bucket.bucket}"
+  key    = "app.js"
+  source = "../app.js"
+  etag   = "${md5(file("../app.js"))}"
+
+  content_type = "text/javascript"
+}
+
+resource "aws_s3_bucket_object" "index_html" {
+  bucket = "${aws_s3_bucket.website_bucket.bucket}"
+  key    = "index.html"
+  source = "../index.html"
+  etag   = "${md5(file("../index.html"))}"
+
+  content_type = "text/html"
+}

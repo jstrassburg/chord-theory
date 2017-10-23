5cc9348b10e8a86c5b3c046cb141e4c506341107
JiM Strassburg <jstrassburg@gmail.com>
2017-10-23 14:11:56 -0500

    secondary dominants

--- app.js
+++ app.js
@@ -24,4 +24,20 @@ angular.module('app', [])
 
         return scale;
     };
+
+    $scope.secondaryDominants = function(){
+        var chords = [];
+        var steps = [2,2,1,2,2,2,1];
+
+        var index = $scope.keyIndex + 7;
+        var degree = 0;
+
+        do {
+            chords[degree] = $scope.allKeys[index % 12] + '7';
+            index += steps[degree % 7];
+            degree++;
+        } while (degree < steps.length - 1);
+
+        return chords;
+    }
 });

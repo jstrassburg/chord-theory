ada3a50e3571c9863fafd27caf0ab0032fffa2b3
JiM Strassburg <jstrassburg@gmail.com>
2017-10-22 23:02:45 -0500

    circle of fifths fixes, re-deploy

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 angular.module('app', [])
 
 .controller('ChordTheory', function($scope){
-    $scope.allKeys = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+    $scope.allKeys = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#/Gb', 'G', 'Ab'];
     $scope.keyIndex = 3;
 
     $scope.updateKey = function(keyIndex) {
         $scope.keyIndex = keyIndex;
     };
 
-    $scope.ionian = function(){
+    $scope.scale = function(steps, decorators){
         var scale = [];
         var index = $scope.keyIndex;
-        steps = [0,2,2,1,2,2,2];
 
         for (degree = 0 ; degree < steps.length ; degree++){
             index += steps[degree];
-            scale[degree] = $scope.allKeys[index % 12]
+            scale[degree] = $scope.allKeys[index % 12] + decorators[degree]
         }
         return scale;
     };
+
+    $scope.ionian = function(){
+        return $scope.scale([0,2,2,1,2,2,2], ['maj7', '-7', '-7', 'maj7', '7', '-7', '-7b5']);
+    }
 });

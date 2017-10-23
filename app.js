70e3c80885d0a5081f575d093fac662b2cc9abbc
JiM Strassburg <jstrassburg@gmail.com>
2017-10-22 22:43:43 -0500

    first scale, ionian, and basic idea of circular queue implemented

--- app.js
+++ app.js
@@ -2,4 +2,21 @@ angular.module('app', [])
 
 .controller('ChordTheory', function($scope){
     $scope.allKeys = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+    $scope.keyIndex = 3;
+
+    $scope.updateKey = function(keyIndex) {
+        $scope.keyIndex = keyIndex;
+    };
+
+    $scope.ionian = function(){
+        var scale = [];
+        var index = $scope.keyIndex;
+        steps = [0,2,2,1,2,2,2];
+
+        for (degree = 0 ; degree < steps.length ; degree++){
+            index += steps[degree];
+            scale[degree] = $scope.allKeys[index % 12]
+        }
+        return scale;
+    };
 });

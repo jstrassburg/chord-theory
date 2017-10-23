1ca821264b49444afec2b1fa44bb8e982e62853a
JiM Strassburg <jstrassburg@gmail.com>
2017-10-22 23:19:47 -0500

    adding dorian

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ angular.module('app', [])
 
         for (degree = 0 ; degree < steps.length ; degree++){
             index += steps[degree];
-            scale[degree] = $scope.allKeys[index % 12] + decorators[degree]
+            scale[degree] = $scope.allKeys[index % 12] + decorators[degree];
         }
         return scale;
     };
 
     $scope.ionian = function(){
         return $scope.scale([0,2,2,1,2,2,2], ['maj7', '-7', '-7', 'maj7', '7', '-7', '-7b5']);
-    }
+    };
+
+    $scope.dorian = function(){
+        return $scope.scale([0,2,1,2,2,2,1], ['-7', '-7', 'maj7', '7', '-7', '-7b5', 'maj7']);
+    };
 });

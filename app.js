694f66d8db15c98fd957c375d2a1be6a0ee42ba8
JiM Strassburg <jstrassburg@gmail.com>
2017-10-23 20:16:57 -0500

    tritone subs

--- app.js
+++ app.js
@@ -48,4 +48,20 @@ angular.module('app', [])
     $scope.neapolitanSixth = function(){
         return $scope.allKeys[($scope.keyIndex + 1) % 12] + '/' + $scope.allKeys[($scope.keyIndex + 5) % 12];
     };
+
+    $scope.tritoneSubstitutions = function(){
+        var chords = [];
+        var steps = [2,2,1,2,2,2,1];
+
+        var index = $scope.keyIndex + 6;
+        var degree = 0;
+
+        do {
+            chords[degree] = $scope.allKeys[index % 12] + '7';
+            index += steps[degree % 7];
+            degree++;
+        } while (degree < steps.length);
+
+        return chords;
+    };
 });
